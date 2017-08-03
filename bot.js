e4211e3f11664962f355339f590f0edd4b47f493
Distkiller <30687583+Distkiller@users.noreply.github.com>
2017-08-03 16:52:59 +0200

    try catch

--- bot.js
+++ bot.js
@@ -21,89 +21,95 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-        var text, intro, wowhead
+    try {
+      if (message.substring(0, 1) == '!') {
+          var args = message.substring(1).split(' ');
+          var cmd = args[0];
+          var text, intro, wowhead
 
-        args = args.splice(1);
+          args = args.splice(1);
 
-        switch(cmd) {
-            //help functions
-            case 'help':
-                text = '!help\n'
-                    + '!ark - Yells out arks favorit word\n'
-                    + '!bin - If you have to bin someone\n'
-                    + '!boe - Show the list of BoE items we have\n'
-                    + '!boeadd - add item !boeadd <type> <piece> <ilvl> <wowheadid> <wowheadroll> - example: !boeadd Plate Helm 915 5851861 155845\n'
-                    + '!boedelete - !boedelete <row> - example for deleting the second row !boedelete 2 \n'
-                    + '!chwazi - !chwazi <numberOfPicks> <name1> ... <nameN> \n'
+          switch(cmd) {
+              //help functions
+              case 'help':
+                  text = '!help\n'
+                      + '!ark - Yells out arks favorit word\n'
+                      + '!bin - If you have to bin someone\n'
+                      + '!boe - Show the list of BoE items we have\n'
+                      + '!boeadd - add item !boeadd <type> <piece> <ilvl> <wowheadid> <wowheadroll> - example: !boeadd Plate Helm 915 5851861 155845\n'
+                      + '!boedelete - !boedelete <row> - example for deleting the second row !boedelete 2 \n'
+                      + '!chwazi - !chwazi <numberOfPicks> <name1> ... <nameN> \n'
+                  fPrint(channelID, text)
+              break;
+
+              // troll message
+              case 'ark':
+                  text = 'Reeeeeeeeeeeeeee'
+                  fPrint(channelID, text)
+              break;
+
+              case 'bin':
+                  text = ':wastebasket::wastebasket::wastebasket: IN THE BIN :wastebasket::wastebasket::wastebasket:'
+                  fPrint(channelID, text)
+              break;
+
+              // bind on equip list
+              case 'boe':
+                if (boe.length > 0) {
+                  intro = "BoE's that are still available: \n"
+                  text = intro + fPrintArray(boe)
+                  fPrint(channelID, text)
+                }
+                else {
+                  text = 'No BoE\'s currently available'
+                  fPrint(channelID, text)
+                }
+              break;
+
+              //Add item to boe list
+              case 'boeadd':
+                wowhead = 'Type: ' + args[0] + ' - Slot: ' + args[1] + " - ilvl: " + args[2] + " - link: http://www.wowhead.com/item=" + args[3] + "&bonus=" + args[4]
+                boe.push(wowhead)
+                text = 'BoE added to the list'
+                fPrint(channelID,text + '\n' + wowhead)
+              break;
+
+              //Delete an item of the boe list
+              case 'boedelete':
+                boe.splice(parseInt(args[0]-1),1)
+                text = 'BoE removed from the list'
                 fPrint(channelID, text)
-            break;
+              break;
 
-            // troll message
-            case 'ark':
-                text = 'Reeeeeeeeeeeeeee'
-                fPrint(channelID, text)
-            break;
+              //Chwazi - pick X number from a list of Options
+              case 'chwazi':
+                var pickCount = args[0]
+                var options = args.splice(1)
+                var choices = []
+                if (pickCount > options.length) {
+                  text = 'Chwazi - You haven\'t given me enough choices. :wastebasket:'
+                  fPrint(channelID, text)
+                }
+                else {
+                  while (pickCount > 0) {
+                      var choice = Math.floor(Math.random() * options.length)
+                      choices.push(options[choice])
+                      pickCount--
+                      options.splice(choice,1)
+                    }
+                    text = 'Chwazi - I have chosen:\n'
+                    fPrint(channelID, text + fPrintArray(choices))
+                }
+              break;
+  
+              // Just add any case commands if you want to..
+           }
+       }
+    }
+    catch (err){
+      fprint(channelid, err.message)
+    }
 
-            case 'bin':
-                text = ':wastebasket::wastebasket::wastebasket: IN THE BIN :wastebasket::wastebasket::wastebasket:'
-                fPrint(channelID, text)
-            break;
-
-            // bind on equip list
-            case 'boe':
-              if (boe.length > 0) {
-                intro = "BoE's that are still available: \n"
-                text = intro + fPrintArray(boe)
-                fPrint(channelID, text)
-              }
-              else {
-                text = 'No BoE\'s currently available'
-                fPrint(channelID, text)
-              }
-            break;
-
-            //Add item to boe list
-            case 'boeadd':
-              wowhead = 'Type: ' + args[0] + ' - Slot: ' + args[1] + " - ilvl: " + args[2] + " - link: http://www.wowhead.com/item=" + args[3] + "&bonus=" + args[4]
-              boe.push(wowhead)
-              text = 'BoE added to the list'
-              fPrint(channelID,text + '\n' + wowhead)
-            break;
-
-            //Delete an item of the boe list
-            case 'boedelete':
-              boe.splice(parseInt(args[0]-1),1)
-              text = 'BoE removed from the list'
-              fPrint(channelID, text)
-            break;
-
-            //Chwazi - pick X number from a list of Options
-            case 'chwazi':
-              var pickCount = args[0]
-              var options = args.splice(1)
-              var choices = []
-              if (pickCount > options.length) {
-                text = 'Chwazi - You haven\'t given me enough choices. :wastebasket:'
-                fPrint(channelID, text)
-              }
-              else {
-                while (pickCount > 0) {
-                    var choice = Math.floor(Math.random() * options.length)
-                    choices.push(options[choice])
-                    pickCount--
-                    options.splice(choice,1)
-                  }
-                  text = 'Chwazi - I have chosen:\n'
-                  fPrint(channelID, text + fPrintArray(choices))
-              }
-            break;
-
-            // Just add any case commands if you want to..
-         }
-     }
 });
 
 //function to print in the channel the user put the command
